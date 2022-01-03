d4ae4b63e136e6c50c3b0cb881cbaf34b31b4c9f
sheniaks <sheniaks@gmail.com>
2022-01-03 23:19:49 +0200

    2.8 Базовые операторы, математика

---
+++ 2.8 Operators/operators.js
@@ -0,0 +1,36 @@
+//Постфиксная и префиксная формы
+// let a = 1, b = 1;
+// let c = ++a; 
+// let d = b++;
+// console.log(`a = ${a}`);
+// console.log(`b = ${b}`);
+// console.log(`c = ${c}`);
+// console.log(`d = ${d}`);
+
+//Результат присваивания
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(`a = ${a}`);
+// console.log(`x = ${x}`);
+
+//Преобразование типов
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+// console.log("  -9  " + 5);
+// console.log("  -9  " - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(" \t \n" - 2);
+
+//Исправьте сложение
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+alert(+a + +b);
